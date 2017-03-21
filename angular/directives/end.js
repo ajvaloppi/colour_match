17eae154a103e7f18c660f67cff0c44dee14fe17
ajvaloppi <amanda.valoppi@gmail.com>
2017-03-21 17:47:15 -0400

    celerbation gif

--- angular/directives/end.js
+++ angular/directives/end.js
@@ -2,7 +2,7 @@
 
 angular.module('end', [])
 
-.controller('endController', ['$scope', '$rootScope', 'AdjusterService', 'MatcherService', function($scope, $rootScope, AdjusterService, MatcherService) {
+.controller('endController', ['$scope', '$rootScope', '$http', 'AdjusterService', 'MatcherService', function($scope, $rootScope, $http, AdjusterService, MatcherService) {
 		$scope.end = false;
 
 		$scope.refreshGame = function () {
@@ -10,11 +10,17 @@ angular.module('end', [])
 			MatcherService.refresh();
 			AdjusterService.refresh();
 			$rootScope.$emit('refresh');
+			$scope.gif = "";
 		}
 
 		$rootScope.$on('doneRound', function (event, data) {
-			$scope.end = true;
 			$scope.correctness = Math.round(AdjusterService.getCorrectness() * 100) / 100;
+
+			$http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=celebrate&rating=pg-13")
+			.then(function(response){ 
+				$scope.gif = response.data.data.image_original_url; 
+				$scope.end = true;
+			});
 		})
 }])
 
