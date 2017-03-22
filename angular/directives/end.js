bfdf7506ac12058c0149a726d9ce8b78d1559e8a
ajvaloppi <amanda.valoppi@gmail.com>
2017-03-22 16:57:09 -0400

    still need to fix percentage off issue

--- angular/directives/end.js
+++ angular/directives/end.js
@@ -4,6 +4,7 @@ angular.module('end', [])
 
 .controller('endController', ['$scope', '$rootScope', '$http', 'AdjusterService', 'MatcherService', function($scope, $rootScope, $http, AdjusterService, MatcherService) {
 		$scope.end = false;
+		$scope.hideGif = false;
 
 		$scope.refreshGame = function () {
 			$scope.end = false;
@@ -15,12 +16,56 @@ angular.module('end', [])
 
 		$rootScope.$on('doneRound', function (event, data) {
 			$scope.correctness = Math.round(AdjusterService.getCorrectness() * 100) / 100;
+			
+			$scope.redDiff = Math.abs(AdjusterService.getRedPercent() - MatcherService.getRedPercent());
+			$scope.greenDiff = Math.abs(AdjusterService.getGreenPercent() - MatcherService.getGreenPercent());
+			$scope.blueDiff = Math.abs(AdjusterService.getBluePercent() - MatcherService.getBluePercent());
 
-			$http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=celebrate&rating=pg-13")
+			var redPercent = MatcherService.getRedPercent();
+			var greenPercent = MatcherService.getGreenPercent();
+			var bluePercent = MatcherService.getBluePercent();
+
+			var blackPercent = 100 - (Math.max(Math.max(redPercent, greenPercent), bluePercent));
+
+		    if (blackPercent != 100) {
+		        cyanPercent = Math.round(((100 - redPercent - blackPercent)/(100 - blackPercent))*100);
+		        magentaPercent = Math.round(((100 - greenPercent - blackPercent)/(100 - blackPercent))*100);
+		        yellowPercent = Math.round(((100 - bluePercent - blackPercent)/(100 - blackPercent))*100);
+		    }
+		    else {
+		        cyanPercent = Math.round(((100 - redPercent - blackPercent)/(100 - 99))*100);
+		        magentaPercent = Math.round(((100 - greenPercent - blackPercent)/(100 - 99))*100);
+		        yellowPercent = Math.round(((100 - bluePercent - blackPercent)/(100 - 99))*100);
+		    }
+
+		    console.log(AdjusterService.getCyan(), AdjusterService.getMagenta(), AdjusterService.getYellow());
+			$scope.cyanDiff = Math.abs(AdjusterService.getCyan() - cyanPercent);
+			$scope.magentaDiff = Math.abs(AdjusterService.getMagenta() - magentaPercent);
+			$scope.yellowDiff = Math.abs(AdjusterService.getYellow() - yellowPercent);
+			$scope.blackDiff = Math.abs(AdjusterService.getBlack() - blackPercent);
+
+			var reaction = ""
+			if ($scope.correctness < 50) {
+				reaction = "oh+no";
+			}
+			else if ($scope.correctness > 75) {
+				reaction = "celebrate";
+			}
+			else {
+				reaction = "you+got+this";
+			}
+
+			console.log(reaction);
+
+			$http.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + reaction + "&rating=pg-13")
 			.then(function(response){ 
 				$scope.gif = response.data.data.image_original_url; 
 				$scope.end = true;
-			});
+			}, function() {
+					$scope.end = true;
+					$scope.hideGif = true;	
+				}
+			);
 		})
 }])
 
